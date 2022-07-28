29210e0a620080d96a0ffa15a3f653d1bc9dcfd9
talhayi <talhayldrm23@gmail.com>
2022-07-28 14:54:45 +0300

    SearchBar added

--- app/src/main/java/com/example/cryptolistcompose/view/CryptoListScreen.kt
+++ app/src/main/java/com/example/cryptolistcompose/view/CryptoListScreen.kt
@@ -1,12 +1,112 @@
 package com.example.cryptolistcompose.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.cryptolistcompose.viewmodel.CryptoListViewModel
 
 @Composable
-fun CyrptoListScreen(navController: NavController){
-    
-    Text(text = "Hello Compose")
-    
+fun CyrptoListScreen(
+    navController: NavController,
+    viewModel: CryptoListViewModel = hiltViewModel()
+) {
+
+    Surface(
+        color = MaterialTheme.colors.secondary,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Text(
+                "Crypto List", modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                textAlign = TextAlign.Center, fontSize = 45.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.primary
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            SearchBar(hint = "Search", modifier = Modifier.fillMaxWidth().padding(16.dp)){
+
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            //List
+        }
+    }
 }
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text = remember {
+        mutableStateOf("")
+    }
+
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+    Box(modifier = modifier){
+        BasicTextField(value = text.value, onValueChange = {
+            text.value = it
+            onSearch(it)
+        }, maxLines = 1,
+            singleLine = true,
+            textStyle = androidx.compose.ui.text.TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(7.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true && text.value.isEmpty()
+                }
+        )
+        if(isHintDisplayed){
+            Text(text = hint,
+                color = Color.LightGray,
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp))
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

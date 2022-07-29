0e7c1acf5f28468b865f5dfa474b00f05b515ccc
talhayi <talhayldrm23@gmail.com>
2022-07-29 17:38:12 +0300

    data listed on the HomeSceen

--- app/src/main/java/com/example/cryptolistcompose/view/CryptoListScreen.kt
+++ app/src/main/java/com/example/cryptolistcompose/view/CryptoListScreen.kt
@@ -1,15 +1,17 @@
 package com.example.cryptolistcompose.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
@@ -21,6 +23,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.cryptolistcompose.model.CryptoList
+import com.example.cryptolistcompose.model.CryptoListItem
 import com.example.cryptolistcompose.viewmodel.CryptoListViewModel
 
 @Composable
@@ -43,11 +47,13 @@ fun CyrptoListScreen(
                 color = MaterialTheme.colors.primary
             )
             Spacer(modifier = Modifier.height(10.dp))
-            SearchBar(hint = "Search", modifier = Modifier.fillMaxWidth().padding(16.dp)){
-
+            SearchBar(hint = "Search", modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)){
+                viewModel.searchCryptoList(it)
             }
             Spacer(modifier = Modifier.height(10.dp))
-            //List
+            CryptoList(navController = navController)
         }
     }
 }
@@ -90,14 +96,78 @@ fun SearchBar(
 
 }
 
+@Composable
+fun CryptoRow(navController: NavController, crypto: CryptoListItem) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = MaterialTheme.colors.secondary)
+        .clickable {
+            navController.navigate("crypto_detail_screen/${crypto.currency}/${crypto.price}")
+        }
+    ) {
+        Text(text = crypto.currency,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary
+            )
+
+        Text(text = crypto.price,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(2.dp),
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+    }
+}
 
 
+@Composable
+fun cryptoListView(cryptos: List<CryptoListItem>,navController: NavController) {
+    
+    LazyColumn(contentPadding = PaddingValues(5.dp)){
+        items(cryptos){crypto->
+            CryptoRow(navController = navController, crypto = crypto)
+        }
+    }
+    
+}
 
+@Composable
+fun CryptoList(navController: NavController,
+                viewModel: CryptoListViewModel= hiltViewModel()) {
+    val cryptoList by remember {viewModel.cryptoList}
+    val errorMessage by remember{viewModel.errorMessage}
+    val isLoading by remember{viewModel.isLoading}
 
+    cryptoListView(cryptos = cryptoList,navController = navController)
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()){
+        if(isLoading){
+            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        }
+        if(errorMessage.isNotEmpty()){
+            RetryView(error = errorMessage) {
+                viewModel.downLoadCryptos()
+            }
+        }
+    }
+}
 
+@Composable
+fun RetryView(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column() {
+        Text(error, color = Color.Red, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = {
+            onRetry
+        }, modifier = Modifier.align(Alignment.CenterHorizontally )) {
 
-
-
+        }
+    }
+}
 
 
 
